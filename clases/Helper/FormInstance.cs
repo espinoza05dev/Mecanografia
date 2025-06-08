b75fe29aee49d39f6eca633c624a5f86cd255106
espinoza05dev <githubpersonal914@gmail.com>
2025-06-08 11:23:08 -0600

    creado para no saturar ESCRITURA.cs y no estar creando nuevas instancias de formularios

---
+++ clases/Helper/FormInstance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MECANOGRAFIA.clases.Helper
+{
+    public  class FormInstance
+    {
+        private List<Form> forms = new List<Form>();
+        public void ShowForm<T>(Action<Form> OwnedForm) where T : Form, new()
+        {
+            T form = forms.OfType<T>().FirstOrDefault();
+            if (form == null)
+            {
+                form = new T();
+                OwnedForm(form);
+                forms.Add(form);
+            }
+            form.ShowDialog();
+        }
+    }
+}
