7e1b278d6954f9f7c40debc1644739a0fc434c61
espinoza05dev <githubpersonal914@gmail.com>
2025-06-08 11:33:33 -0600

    se añadieron mas campos para login o registor de usuario

--- clases/Helper/User.cs
+++ clases/Helper/User.cs
@@ -3,11 +3,16 @@
     public class User
     {
         public int dias_seguido { get; set; }
+        public string usuario { get; set; }
+        public string usuariocmb { get; set; }
+        public string contra { get; set; }
+        public string user2 { get; set; }
+        public string contra2 { get; set; }
         public string tabla { get; set; }
         public string campos { get; set;}
         public string valores { get; set;}
         public string condicion  { get; set; }
-        public string msg { get; set; }
+        public string msg { get; set; } = "¿Desea Cerrar Sesion?";
 
     }
 }
