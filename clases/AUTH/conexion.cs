44f3995f686758a0fbd7fbb8c51d9c7d76ae5508
espinoza05dev <githubpersonal914@gmail.com>
2025-06-08 11:20:11 -0600

    se puede conectar de diversas maneras a la base de datos

--- clases/AUTH/conexion.cs
+++ clases/AUTH/conexion.cs
@@ -7,8 +7,36 @@ namespace MECANOGRAFIA.clases
 {
     internal class conexion:ENV
     {
+        static string ObtenerCadenaConexion()
+        {
+            string[] testConnections = { 
+                $"Data Source={Dns.GetHostName()};Initial Catalog=MECANOGRAFIAA;Integrated Security=True;Connection",
+                $"Data Source={Dns.GetHostName()}\\SQLEXPRESS;Initial Catalog=MECANOGRAFIAA;Integrated Security=True;Connection",
+                $"Data Source=.;Initial Catalog=MECANOGRAFIAA;Integrated Security=True;Connection",
+                $"Data Source=.\\SQLEXPRESS;Initial Catalog=MECANOGRAFIAA;Integrated Security=True;Connection"
+                };
+
+            foreach (string connStr in testConnections)
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connStr))
+                    {
+                        conn.Open();
+                        return connStr.Replace(";Connection", "");
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+
+            return "Data Source=" + Dns.GetHostName() + "\\SQLEXPRESS;Initial Catalog=MECANOGRAFIAA;Integrated Security=True";
+        }
+
         // CADENA DE CONEXION PARA CONEXIONES LOCALES //static string cadena_de_conexion = $"Server=" + ENV.SERVER + ";Database=" + ENV.DB + ";User Id=" + ENV.USER + ";Password=" + ENV.PWD + ";TrustServerCertificate=True";
-        static string cadena_conexion = "Data Source=" + Dns.GetHostName() + "\\LOCAL;Initial Catalog=MECANOGRAFIAA;Integrated Security=True";
+        static string cadena_conexion = ObtenerCadenaConexion();
         public static SqlConnection SQLCon = new SqlConnection(cadena_conexion);
 
         public void Abrirconexion()
